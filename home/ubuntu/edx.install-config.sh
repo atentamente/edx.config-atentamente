2dbecd91406d713b3d1e6c7bdd43c98dac8787c9
Lawrence McDaniel <lpm0073@gmail.com>
2019-04-27 07:53:31 -0500

    update admin scripts

---
+++ home/ubuntu/edx.install-config.sh
@@ -0,0 +1,41 @@
+# written by: lawrence mcdaniel
+#             https://lawrencemcdaniel.com
+#             https://blog.lawrencemcdaniel.com
+#
+# date:       feb-2018
+#
+# usage:      1. install open edx lms + cms config files
+#             2. nginx custom configurations
+#             3. ansible customizations
+#             4. install custom theme.
+#             5. django LMS/CMS configuration overrides
+#             6. admin scripts
+#---------------------------------------------------------
+
+rm -rf /home/ubuntu/edx.config-atentamente
+git clone https://github.com/atentamente/edx.config-atentamente.git
+
+echo 1. open edx LMS + CMS configuration
+sudo cp /home/ubuntu/edx.config-atentamente/edx/app/edxapp/*.json /edx/app/edxapp/
+sudo chown -R edxapp /edx/app/edxapp/*.json
+sudo chgrp -R www-data /edx/app/edxapp/*.json
+
+#echo 2. Nginx custom configurations
+# these contain lets encrypt ssl certificate and https redirection
+sudo cp /home/ubuntu/edx.config-atentamente/edx/app/nginx/lms /edx/app/nginx/sites-available/
+sudo cp /home/ubuntu/edx.config-atentamente/edx/app/nginx/cms /edx/app/nginx/sites-available/
+
+#echo 3. Ansible customizations
+# copy server-vars and any other mods to ansible-related work flows
+sudo cp /home/ubuntu/edx.config-atentamente/edx/app/edx_ansible/*.* /edx/app/edx_ansible/edx_ansible/
+
+#echo 4. install custom theme
+sudo cp -R /home/ubuntu/edx.config-atentamente/themes/atentamente /edx/app/edxapp/edx-platform/themes/
+sudo chown -R edxapp /edx/app/edxapp/edx-platform/themes/
+sudo chgrp -R www-data /edx/app/edxapp/edx-platform/themes/
+
+#echo 5. django configuration files
+# nothing to do
+
+#echo 6. admin scripts
+#sudo cp -R /home/ubuntu/edx.config-atentamente/home/ubuntu/* /home/ubuntu/
--- home/ubuntu/edx.platform-upgrade.sh
+++ home/ubuntu/edx.platform-upgrade.sh
@@ -1,39 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+# Update edx-platform fork
 # McDaniel
-# April 2018
+# Feb 2019
 #
-# Usage:     To upgrade an existing Native build.
-#            Native Open edX Ubuntu 16.04 64 bit Installation
+# Performs a refresh of the code base in edx-platform.
 #
-# Reference: Native Open edX Ubuntu 16.04 64 bit Installation
-#            https://openedx.atlassian.net/wiki/spaces/OpenOPS/pages/146440579/Native+Open+edX+Ubuntu+16.04+64+bit+Installation
 #
-# This script takes around 2 hours to complete. It is intended to be run unattended, on a background thread using nohup.
+# This script takes around 40 minutes to complete.
+#---------------------------------------------------------
 
 
 cd ~
-#sudo rm edx.platform-upgrade.out
 
-# Upgrade to the absolute most recent code base. thus far i've found this more reliable than named releases.
-export OPENEDX_RELEASE=master
 
-# Completely shut down all edX services
-sudo /edx/bin/supervisorctl stop edxapp:
-sudo /edx/bin/supervisorctl stop edxapp_worker:
-
-sudo /edx/bin/supervisorctl stop analytics_api
-sudo /edx/bin/supervisorctl stop certs
-sudo /edx/bin/supervisorctl stop discovery
-sudo /edx/bin/supervisorctl stop ecommerce
-sudo /edx/bin/supervisorctl stop ecomworker
-sudo /edx/bin/supervisorctl stop forum
-sudo /edx/bin/supervisorctl stop insights
-sudo /edx/bin/supervisorctl stop notifier-celery-workers
-sudo /edx/bin/supervisorctl stop notifier-scheduler
-sudo /edx/bin/supervisorctl stop xqueue
-sudo /edx/bin/supervisorctl stop xqueue_consumer
-
-# Upgrade Open edX:
-# See: https://openedx.atlassian.net/wiki/spaces/OpenOPS/pages/60227913/Managing+OpenEdX+Tips+and+Tricks
-sudo /edx/bin/update edx-platform master 
-
+./edx.platform-stop.sh
+rm -rf /edx/app/edxapp/edx-platform
+/edx/bin/update edx-platform open-release/ironwood.master
+./edx.install-config.sh
+./edx.compile-assets.sh
+./edx.platform-restart-full.sh
